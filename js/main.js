6c96a31ac6b230e7fd1b8c065ac7ee5143fda9e3
Therakar <matteo.pau200@gmail.com>
2022-11-15 16:51:57 +0100

    inserisco l'array delle task nel js e lo stampo nell'html  grazie a v-for

--- js/main.js
+++ js/main.js
@@ -5,7 +5,20 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        elementsList: [
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare l\'esercizio',
+                done: false
+            },
+            {
+                text: 'Riordinare la casa',
+                done: false
+            },
+        ]
       }
     }
-  }).mount('#app')
+  }).mount('#app');
