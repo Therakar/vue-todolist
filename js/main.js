f2243ef35245153c74c7dbb41306f2c2de000c61
Therakar <matteo.pau200@gmail.com>
2023-04-08 19:09:23 +0200

    Permanenza dei dati
     i dati della to-do list vengono salvati nello storage locale utilizzando la funzione localStorage.setItem() e caricati utilizzando la funzione localStorage.getItem() al momento del montaggio del componente Vue e dopo ogni operazione di aggiunta, rimozione o aggiornamento di un elemento. In questo modo, i dati della to-do list saranno memorizzati nello storage locale del browser anche dopo la chiusura dell'applicazione e verranno caricati nuovamente al momento del successivo avvio dell'applicazione.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,57 @@
-"use strict"
+"use strict";
 
-const { createApp } = Vue
+const { createApp } = Vue;
 
-    createApp({
-        data() {
-        return {
+createApp({
+  data() {
+    return {
+      // oggetto vuoto che viene riempito dall'input dell'utente
+      newElement: {
+        text: "",
+        done: false,
+      },
 
-            // oggetto vuoto che viene riempito dall'input dell'utente
-            newElement: {
-                text: '',
-                done: false
-            },
+      // array di oggetti che contiene le tasks
+      elementsList: [],
+    };
+  },
 
-            // array di oggetti che contiene le tasks
-            elementsList: []
-        }
-        },
-
-        methods: {
-
-            // funzione che pusha l'oggetto inserito dall'utente e poi svuota la variabie in modo che possa essere riutlizzata
-            addElement() {
-                this.elementsList.push(this.newElement); //pusha il nuovo elemento
-                console.log(this.newElement) 
-                this.newElement= {
-                    text: '',
-                    done: false
-                }; //svuota la variabile newElement
-            },
-            removeElement(i){
-                this.elementsList.splice(i, 1);  
-            },
-            completedTask(i){
-                if (this.elementsList[i].done === false){ //SE l'elemento done ha valore false al momento del click
-                  this.elementsList[i].done = true; //il valore viene cambiato a true
-                } else {
-                  this.elementsList[i].done = false;//ALTRIMETNI viene cambiato a false
-                }
-              },
-        }
-}).mount('#app')
-
-    
+  methods: {
+    // funzione che pusha l'oggetto inserito dall'utente e poi svuota la variabile in modo che possa essere riutilizzata
+    addElement() {
+      this.elementsList.push(this.newElement); // pusha il nuovo elemento
+      console.log(this.newElement);
+      this.newElement = {
+        text: "",
+        done: false,
+      }; // svuota la variabile newElement
+      this.saveDataLocalStorage(); // salva i dati nello storage locale
+    },
+    removeElement(i) {
+      this.elementsList.splice(i, 1);
+      this.saveDataLocalStorage(); // salva i dati nello storage locale dopo la rimozione di un elemento
+    },
+    completedTask(i) {
+      if (this.elementsList[i].done === false) {
+        // SE l'elemento done ha valore false al momento del click
+        this.elementsList[i].done = true; // il valore viene cambiato a true
+      } else {
+        this.elementsList[i].done = false; // ALTRIMENTI viene cambiato a false
+      }
+      this.saveDataLocalStorage(); // salva i dati nello storage locale dopo l'aggiornamento di un elemento
+    },
+    // Funzione per salvare i dati nello storage locale
+    saveDataLocalStorage() {
+      localStorage.setItem("toDoList", JSON.stringify(this.elementsList));
+    },
+    // Funzione per caricare i dati dallo storage locale
+    uploadDataLocalStorage() {
+      const dati = localStorage.getItem("toDoList");
+      this.elementsList = dati ? JSON.parse(dati) : [];
+    },
+  },
+  mounted() {
+    // Carica i dati della to-do list dallo storage locale al momento del montaggio del componente
+    this.uploadDataLocalStorage();
+  },
+}).mount("#app");
